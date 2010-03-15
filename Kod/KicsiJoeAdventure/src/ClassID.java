3538f2ef2ae773d6ce69af24357d97c1151716dd
Zsuzsi <shamanic.sue@gmail.com>
2010-03-15 01:05:07 +0100

    Na most talán végre
    Signed-off-by: Zsuzsi <shamanic.sue@gmail.com>

---
+++ Kod/KicsiJoeAdventure/src/ClassID.java
@@ -0,0 +1,23 @@
+
+
+/**
+ * Class ClassID:
+ * Segédosztály a szkeleton verzióhoz.
+ * Minden osztály örököl tőle, így adható nekik "objektum név".
+ */
+public abstract class ClassID {
+
+    //Az objektum neve
+    protected String ID;
+
+    //Az objektumnév beállítása
+    public void setID(String s) {
+        ID = new String(s);
+    }
+
+    //Az objektumnév lekérdezése
+    @Override
+    public String toString(){
+        return ID;
+    }
+}
