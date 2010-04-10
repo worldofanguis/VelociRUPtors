2e61f98c0ddca19f99a6981d69b1dadef3df1bf0
Ferdinand <ferdi89@gmail.com>
2010-04-10 23:09:55 +0200

    * upsz :D
    Signed-off-by: Ferdinand <ferdi89@gmail.com>

---
+++ Kod/Compare/Compare run.bat
@@ -0,0 +1,2 @@
+@echo off
+java -jar dist\compare.jar RabloVsHideoutOn_resROSSZ.txt ../../Teszt/RabloVsHideoutOn_res.txt
---
+++ Kod/Compare/src/Main.java
@@ -0,0 +1,68 @@
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
+/**
+ * Összehasonlító program. Parancssori argumentumokként
+ * kapja meg az összehasonlítandó fájlok elérési címét,
+ * amennyiben egyeznek, kiírja, hogy OK, egyébként pedig kiírja
+ * az első hibás sort, amit talált.
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String kapott;
+        String elvart;
+        try {
+            kapott = args[0];
+            elvart = args[1];
+            
+            BufferedReader result = new BufferedReader(new FileReader(new File(kapott)));
+            BufferedReader expected = new BufferedReader(new FileReader(new File(elvart)));
+
+            String lineR, lineE;
+            boolean good = true;
+            while ( ( ( lineR = result.readLine() ) != null ) && ( ( lineE = expected.readLine() ) != null ) )
+            {
+                while ( lineR.startsWith("#") || lineR.isEmpty() )
+                    lineR = result.readLine();
+                while ( lineE.startsWith("#") || lineE.isEmpty() )
+                    lineE = expected.readLine();
+
+                if ( lineR.compareTo(lineE) == 0 )
+                    continue;
+                else {
+                    System.out.println("Kapott: " + lineR );
+                    System.out.println("Vart: " + lineE );
+                    good = false;
+                }
+            }
+            if (good) System.out.println("OK");
+            else System.out.println("HIBA");
+        } catch (ArrayIndexOutOfBoundsException e)
+        {
+            System.out.println("Tul keves argumentum");
+            System.exit(-1);
+        } catch (FileNotFoundException e)
+        {
+            System.out.println("Nem talalhato fajl");
+            System.exit(-1);
+        } catch (Exception e)
+        {
+            System.out.println("Hibbba.");
+            System.exit(-1);
+        }
+        
+        //Ize a vegere hogy ne menekuljon el
+        try {
+            int i = System.in.read();
+        } catch (Exception e) {
+            System.out.println("well, duh");
+        }
+    }
+}
