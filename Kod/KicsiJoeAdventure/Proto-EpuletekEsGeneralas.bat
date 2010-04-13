d457371432b7171cd0ee1ec9944ddcf41a092bab
Zsuzsi <shamanic.sue@gmail.com>
2010-04-14 00:16:56 +0200

    Semmi lenyeges.
    Signed-off-by: Zsuzsi <shamanic.sue@gmail.com>

--- Kod/KicsiJoeAdventure/Proto-EpuletekEsGeneralas.bat
+++ Kod/KicsiJoeAdventure/Proto-EpuletekEsGeneralas.bat
@@ -5,13 +5,14 @@ echo Rablo vs. Bank (nincs kirabolva)
 java -jar ..\Compare\dist\compare.jar ../../Teszt/RabloVsBankOff_out.txt ../../Teszt/RabloVsBankOff_res.txt
 
 echo Rablo vs. Bank (ki van rabolva)
-
+java -jar ..\Compare\dist\compare.jar ../../Teszt/RabloVsBankOn_out.txt ../../Teszt/RabloVsBankOn_res.txt
 
 echo Rablo vs. Rejtekhely (nincs kirabolva)
+java -jar ..\Compare\dist\compare.jar ../../Teszt/RabloVsHideoutOff_out.txt ../../Teszt/RabloVsHideoutOff_res.txt
 
 
 echo Rablo vs. Rejtekhely (ki van rabolva)
-
+java -jar ..\Compare\dist\compare.jar ../../Teszt/RabloVsHideoutOn_out.txt ../../Teszt/RabloVsHideoutOn_res.txt
 
 echo Civil vs. Epulet
 
